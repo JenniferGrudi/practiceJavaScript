7a79a5290bf77397f6d0b6ab6f072d16130f585f
jennifergrudi <jen.grudi@gmail.com>
2017-09-13 21:57:40 -0400

    finished 3 question challenge

--- challenge.js
+++ challenge.js
@@ -8,78 +8,83 @@
 
 
 //this is my constructor in a function
-var Question = function(question, answer, realAnswer){
-  this.question = question; 
-  this.answer = answer;
-  this.realAnswer = realAnswer;
-}
 
-//2. create a couple of questions using the constructor...
 
-//how to make answers an array?
-//a = ['a1', 'a2', 'a3']
-
-//function(var question, var answer, var rightAnswer)
-
-var question1 = new Question('What is my name?', ['Jennifer', 'Nancy', 'Stacy'], 0);
-
-var question2 = new Question('What is my favorite color?', ['black', 'green', 'pink', 'blue'], 1);
-
-var question3 = new Question('what is my favorite holiday?', ['Christmas', 'Thanksgiving', 'Halloween', 'Easter'], 2);
-
-//3. store all questions inside an array
-
-var arrayOfQuestions = [question1, question2, question3];
-
-//4.) Select on random question and log it on the console, together with the possible answer(each question should have a number)
-
-  //Math.floor takes the decimal returns the less than or equal to a given number.  Example: Math.floor(1.2) will give you 1  
-
-  //Math.random() picks a random number with a decimal.  * numbers you want to randomly pick from.  example: Math.random() * 3 will pick numbers from 1-3.  The number may be objects?
-
-//q = Math.floor(Math.random() * 3);
-//console.log(q); //this gives me a result of NaN - Not a Number
-
-q = Math.floor(Math.random() * arrayOfQuestions.length)
-// console.log(q);
-
-//method using constuctor prototype in order to display questions
-
-Question.prototype.randomQuestion = function() {
-  //need to log questions
-  console.log('Question: ' + this.question);
-
-  for (var i = 0; i < this.answer.length; i++) { 
-    console.log(i + ': ' + this.answer[i]);
+  var Question = function(question, answer, realAnswer){
+    this.question = question; 
+    this.answer = answer;
+    this.realAnswer = realAnswer;
   }
-};
 
-arrayOfQuestions[q].randomQuestion();
+  //2. create a couple of questions using the constructor...
 
-//5. Use the 'prompt' functin to ask the user for the correct answer. The user should input the number of the correct answer.  
+  //how to make answers an array?
+  //a = ['a1', 'a2', 'a3']
 
-var guess = prompt('Please take a guess from the answers...');
+  //function(var question, var answer, var rightAnswer)
 
-//6. check if the answer is correct and print to the console whether the answer is correct or not 
+  var question1 = new Question('What is my name?', ['Jennifer', 'Nancy', 'Stacy'], 0);
 
-//another method using the constructor
-//i need to pass in something to this function cause i need to compare it with my answers
+  var question2 = new Question('What is my favorite color?', ['black', 'green', 'pink', 'blue'], 1);
 
-Question.prototype.checkAnswer = function(guess) {
-  if(guess === this.realAnswer){
-    alert('You are correct!  You DO know me :)');
-  } else {
-    alert('NOPE! maybe next time');
-  }
-};
+  var question3 = new Question('what is my favorite holiday?', ['Christmas', 'Thanksgiving', 'Halloween', 'Easter'], 2);
 
-arrayOfQuestions[q].checkAnswer();
+  //3. store all questions inside an array
+
+  var arrayOfQuestions = [question1, question2, question3];
+
+  //4.) Select on random question and log it on the console, together with the possible answer(each question should have a number)
+
+    //Math.floor takes the decimal returns the less than or equal to a given number.  Example: Math.floor(1.2) will give you 1  
+
+    //Math.random() picks a random number with a decimal.  * numbers you want to randomly pick from.  example: Math.random() * 3 will pick numbers from 1-3.  The number may be objects?
+
+  //q = Math.floor(Math.random() * 3);
+  //console.log(q); //this gives me a result of NaN - Not a Number
+
+  q = Math.floor(Math.random() * arrayOfQuestions.length)
+  // console.log(q);
+
+  //method using constuctor prototype in order to display questions
+
+  Question.prototype.randomQuestion = function() {
+    //need to log questions
+    console.log('Question: ' + this.question);
+
+    for (var i = 0; i < this.answer.length; i++) { 
+      console.log(i + ': ' + this.answer[i]);
+    }
+  };
+
+  arrayOfQuestions[q].randomQuestion();
+
+  //5. Use the 'prompt' functin to ask the user for the correct answer. The user should input the number of the correct answer.  
+
+  var guess = prompt('Please guess the correct answers...');
+
+  //6. check if the answer is correct and print to the console whether the answer is correct or not 
+
+  //another method using the constructor
+  //i need to pass in something to this function cause i need to compare it with my answers
+
+  Question.prototype.checkAnswer = function(answr) {
+    // console.log(guess);
+    // console.log(this.realAnswer);
+    if(answr == this.realAnswer){
+
+      console.log('You are correct!  You DO know me :)');
+    } else {
+      console.log('NOPE! maybe next time');
+    }
+  };
+
+  arrayOfQuestions[q].checkAnswer(guess);
 
 
 
 
 
-//7. suppose this code would be plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (hint: we learned a special technique to do exactly that).
+  //7. suppose this code would be plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (hint: we learned a special technique to do exactly that).
 
 
 
